a82f0bbc54622642bd7b3bb76138284ee7124a5e
Hitesh <Hiteshindia533@gmail.com>
2024-01-31 20:02:59 +0530

    New javaFile commit 1

--- newfolder.java/newfolderfile.java
+++ newfolder.java/newfolderfile.java
@@ -1,3 +1,7 @@
-class newfolderfile{
-    
+class newfolderfile {
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 25;
+        System.out.println(a + b);
+    }
 }
